5e403c4d80a531977af177e93e1a86907dcdaa6d
Lauren Milechin <lauren.milechin@mit.edu>
2018-05-23 09:37:45 -0400

    remove octave specific code

--- src/matlab-octave/octave-compatible/cellContains.m
+++
@@ -1,8 +0,0 @@
-function [res] = cellContains(cellArray,str)
-%UNTITLED10 Summary of this function goes here
-%   Detailed explanation goes here
-
-res = ~cellfun(@isempty,strfind(cellArray,str)) ;
-
-end
-
